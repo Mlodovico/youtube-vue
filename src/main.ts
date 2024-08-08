646de789dcba50ef4d2bf99663c59e3594cb4472
mlodovico <murilo.lodovico@outlook.com>
2024-08-08 17:24:49 -0300

    feature: Header component and some styles to fix responsive layout

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,19 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { OhVueIcon, addIcons } from 'oh-vue-icons'
+import { FaFlag, RiZhihuFill } from 'oh-vue-icons/icons'
+
 
 import App from './App.vue'
 import router from './router'
 
+addIcons(FaFlag, RiZhihuFill)
+
 const app = createApp(App)
 
 app.use(createPinia())
+app.component('v-icon', OhVueIcon)
 app.use(router)
 
 app.mount('#app')
