d3a7c7b5597f6394050ecd45a05137c5b6f029d8
mlodovico <murilo.lodovico@outlook.com>
2024-09-22 21:23:26 -0300

    feature: creating side bar component

---
+++ src/components/layout/SideBarComponent.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    name: 'SideBarComponent',
+
+    data() {
+        return {
+            search: ''
+        }
+    },
+
+    methods: {
+        onClick() {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="side-bar-container">
+
+    </div>
+</template>
+
+<style>
+.side-bar-container {
+    width: 300px;
+    height: 100vh;
+    background-color: rgb(30, 30, 30);
+}
+</style>
